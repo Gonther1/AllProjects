ef196c31cbbef1bce4ee798a65b1f6e6ed962f35
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-07-26 19:40:57 -0500

    a

---
+++ pythonProjects/reto1/principal.py
@@ -0,0 +1,37 @@
+import os
+import pacientes
+import veterinarios
+import citas
+if __name__ == "__main__":
+    isActivate = True
+    opcion=0
+    bandera=True
+    while bandera:
+        try:
+            while isActivate:
+                os.system("cls")
+                print('+','-'*74,'+')
+                print("|{:^20}{}{:^19}|".format(' ','ADMINISTRACION DEL CENTRO VETERINARIO',' '))
+                print('+','-'*74,'+')
+                print("1. Gestion de pacientes")
+                print("2. Gestion de veterinarios")
+                print("3. Gestion de citas medicas")
+                print("4. Salir")
+                opcion = int(input(":)_"))
+                if (opcion == 1):
+                    pacientes.MainMenu()
+                elif (opcion == 2):
+                    veterinarios.MainMenu()
+                elif (opcion == 3):
+                    citas.MainMenu()
+                elif (opcion == 4):
+                    isActivate=False  
+                    bandera=False
+                else:
+                    os.system("cls")
+                    print(input("La opcion ingresada no es valida\n\nPresiona una tecla para continuar..."))        
+        except:
+            os.system("cls")
+            print(input("La opcion ingresada no es valida\n\nPresiona una tecla para continuar..."))
+      
+    
